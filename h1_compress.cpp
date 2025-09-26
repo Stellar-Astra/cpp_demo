9589eb582cff5cf3221eda7b7f58cd3dcddebdc9
Stellar-Astra <209887959+Stellar-Astra@users.noreply.github.com>
2025-09-26 21:18:27 +1000

    Initial Commit

---
+++ h1_compress.cpp
@@ -0,0 +1,34 @@
+#include "splashkit.h"
+     
+using std::string;
+using std::to_string;
+
+// Constant for number of memories 
+
+
+// Enum for memory genres 
+
+
+// Memory struct
+
+
+// Function to compress memory content, passing by reference
+
+
+// Function to decompress memory content, passing by reference
+
+
+// Function to generate demo memories
+
+
+// Function to show compression statistics, passing by reference
+
+
+// Function to collapse and preview a few memories, passing by reference
+
+
+// Function to find memories by genre, passing by reference 
+
+
+// Main function 
+int main()
